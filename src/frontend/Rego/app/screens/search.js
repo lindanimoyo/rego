74a09c0898c131169c6e164c570513b3b4090ae7
totodingi <mudakiwilson@yahoo.com>
2022-04-03 16:20:13 +0300

    Added a search screen boilerplate code

---
+++ src/frontend/Rego/app/screens/search.js
@@ -0,0 +1,23 @@
+import * as React from 'react';
+import {
+	View,
+	TouchableOpacity,
+	Text
+} from "react-native";
+
+function Search(props) {
+	return(
+		<>
+			<View
+				style={{
+					backgroundColor: '#fff',
+					flex:1
+				}}
+			>
+
+			</View>
+		</>
+	)
+}
+
+export default Search;
