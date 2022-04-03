60b785741ee2e8ca95aba276e0ec0028a664b60b
totodingi <mudakiwilson@yahoo.com>
2022-04-03 17:08:12 +0300

    Added a main app entry point file and set up apollo-client and redux state manager

---
+++ src/frontend/Rego/app/main.js
@@ -0,0 +1,68 @@
+import * as React from 'react';
+import {createStore} from "redux";
+import {Provider} from "react-redux";
+import rootReducer from "./state/index"
+import {ApolloClient, ApolloProvider, InMemoryCache} from "@apollo/client";
+import Publications from "./screens/publications";
+import Account from "./screens/account";
+import Search from "./screens/search";
+import Login from "./screens/Login";
+import Signup from "./screens/signup";
+import {Router, Scene, Lightbox, Tabs} from "react-native-router-flux";
+import CustomTabBar from "./components/tab";
+
+function App(){
+	const store = createStore(rootReducer);
+	const client = new ApolloClient({
+		link: "https://rego.org/graphql",
+		cache: new InMemoryCache()
+	})
+	return (
+		<>
+			<ApolloProvider client={client}>
+				<Provider store={store}>
+					<Router>
+						<Lightbox>
+							<Scene key={'root'}>
+								<Tabs
+									key={'tabBar'}
+									tabBarComponent={CustomTabBar}
+									hideNavBar
+									lazy
+								>
+									<Scene
+										component={Publications}
+										key={'publications'}
+										hideNavBar
+									/>
+									<Scene
+										component={Search}
+										key={'search'}
+										hideNavBar
+									/>
+									<Scene
+										component={Account}
+										key={'account'}
+										hideNavBar
+									/>
+								</Tabs>
+								<Scene
+									component={Login}
+									key={'login'}
+									hideNavBar
+								/>
+								<Scene
+									component={Signup}
+									key={'signup'}
+									hideNavBar
+								/>
+							</Scene>
+						</Lightbox>
+					</Router>
+				</Provider>
+			</ApolloProvider>
+		</>
+	)
+}
+
+export default App;
