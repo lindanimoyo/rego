ba33aed8ac93b439585ef9cf0751eb388a7f9fdf
totodingi <mudakiwilson@yahoo.com>
2022-04-03 16:22:07 +0300

    Added an account screen boilerplate code

---
+++ src/frontend/Rego/app/screens/account.js
@@ -0,0 +1,23 @@
+import * as React from 'react';
+import {
+	View,
+	TouchableOpacity,
+	Text
+} from "react-native";
+
+function Account(props) {
+	return(
+		<>
+			<View
+				style={{
+					backgroundColor: '#fff',
+					flex:1
+				}}
+			>
+
+			</View>
+		</>
+	)
+}
+
+export default Account;
