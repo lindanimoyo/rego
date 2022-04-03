e3c5359cb1c5948e18e220ea1702e05eb223d6d9
totodingi <mudakiwilson@yahoo.com>
2022-04-03 16:26:24 +0300

    Added a login screen boilerplate code

---
+++ src/frontend/Rego/app/screens/Login.js
@@ -0,0 +1,23 @@
+import * as React from 'react';
+import {
+	View,
+	TouchableOpacity,
+	Text
+} from "react-native";
+
+function Login(props) {
+	return(
+		<>
+			<View
+				style={{
+					backgroundColor: '#fff',
+					flex:1
+				}}
+			>
+
+			</View>
+		</>
+	)
+}
+
+export default Login;
