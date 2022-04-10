01cfa1aee666387167b0299f2f7622ae02533c85
totodingi <mudakiwilson@yahoo.com>
2022-04-10 10:59:06 +0300

    Added an abstract screen

---
+++ src/frontend/Rego/app/screens/abstract.js
@@ -0,0 +1,36 @@
+import * as React from 'react';
+import {
+  View,
+  TouchableOpacity,
+  Text
+} from "react-native";
+import {bindActionCreators} from "redux";
+import {connect} from "react-redux";
+
+function Abstract(props) {
+  return(
+    <>
+      <View
+        style={{
+          backgroundColor: '#fff',
+          flex:1
+        }}
+      >
+
+      </View>
+    </>
+  )
+}
+
+
+const mapStateToProps = state => {
+  const {app} = state;
+  return {app}
+}
+
+const mapDispatchToProps = dispatch => (
+  bindActionCreators({
+  }, dispatch)
+)
+
+export default connect(mapStateToProps, mapDispatchToProps)(Abstract)
