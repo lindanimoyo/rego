1337ea14662c9aca5a51014e72cf7b315218174b
totodingi <mudakiwilson@yahoo.com>
2022-04-12 01:32:09 +0300

    Added a splash screen

---
+++ src/frontend/Rego/app/screens/splash.js
@@ -0,0 +1,24 @@
+import * as React from 'react';
+import {View} from 'react-native';
+import {bindActionCreators} from 'redux';
+import {connect} from 'react-redux';
+import Loading from "../components/loading";
+
+function Splash(props) {
+  return (
+    <>
+      <View style={{flex: 1, backgroundColor: '#ff0092', alignItems: 'center'}}>
+        <Loading {...props} />
+      </View>
+    </>
+  );
+}
+
+const mapStateToProps = state => {
+  const {app} = state;
+  return {app};
+};
+
+const mapDispatchToProps = dispatch => bindActionCreators({}, dispatch);
+
+export default connect(mapStateToProps, mapDispatchToProps)(Splash);
